6d87ae2ca3c386fb8b7981b046e914e42bf52da8
Hao Li <lihao9121@gmail.com>
2020-09-20 15:16:23 -0700

    comment

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -3,17 +3,18 @@ import * as api from '../api/profile';
 import { GET_PROFILE } from '../constants/actions';
 
 export function getUserProfile(payload = {}) {
-  return async (dispatch) => {
+  return async (dispatch) => { // redux-thunk middleware, this dispatch get from middleware, redux-thunk
     console.log('test getUserProfile');
     const {
       code,
       message: msg,
       data } = await api.getUserProfile(payload);
     if (code === 0) {
-      dispatch({ // dispatch here 一整个是个action
+      dispatch({ // dispatch here 一整个是个action, this one goes to reducer
         type: GET_PROFILE,
         payload: data,
       })
+      // dispatch(); // can also do another action like this one get called
     } else {
       message.error(msg);
     }
